d1e09093e83b919f096d435f7a1ee6d522a1aecd
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2024-07-09 20:50:32 +0530

    prisma db setup with postgresql

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express, { Express, Request, Response } from 'express';
+import 'dotenv/config';
+import prisma from './prismadb';
 
 const app: Express = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!!!');
+app.get('/', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.send(`Hi there! We have ${users.length} users in the database.`);
+});
+
+app.get('*', (req: Request, res: Response) => {
+  res.send('404 - Not Found');
 });
 
 app.listen(port, () => {
